fae7cf6e79e5ec762c6886f5a898cdd9207d19d0
Julliane <dipaulajulliane@gmail.com>
2026-01-30 20:33:39 -0300

    feat: api structure with docker, drizzle and fastify

---
+++ api/src/server.ts
@@ -0,0 +1,40 @@
+import { fastify } from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifyCors } from '@fastify/cors'
+import ScalarApiReference from '@scalar/fastify-api-reference'
+import { listWebhooks } from './routes/list-webhooks'
+import { env } from './env'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Webhook Inspector API',
+      description: 'API for capturing and inspecting webhooks in Webhook requests',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(ScalarApiReference, {
+  routePrefix: '/docs',
+})
+
+app.register(listWebhooks)
+
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
+  console.log(`🔥 HTTP Server running on http://localhost:${env.PORT}!`)
+  console.log(`📚 Docs available at http://localhost:${env.PORT}/docs`)
+})
